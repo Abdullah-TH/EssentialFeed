bbc770592eb6611bc19207966e2b90163fba72a1
Abdullah Mosleh Althobetey <aalthobetey@elm.sa>
2019-08-06 11:49:36 +0300

    Create FeedItem Model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Abdullah Althobetey on 06/08/2019.
+//  Copyright © 2019 AbdullahTH. All rights reserved.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let image: URL
+}
