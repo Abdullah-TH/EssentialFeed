9b39d7f0562492b112bfad10ca129212e20332c2
Abdullah Althobetey <abdullah.th@me.com>
2019-12-27 19:23:40 +0300

    `LocalFeedLoader` in integration with `CodableFeedStore` delivers no items on empty cache

--- EssentialFeedCacheIntergrationTests/EssentialFeedCacheIntergrationTests.swift
+++ EssentialFeedCacheIntergrationTests/EssentialFeedCacheIntergrationTests.swift
@@ -7,9 +7,45 @@
 //
 
 import XCTest
+import EssentialFeed
 
 class EssentialFeedCacheIntergrationTests: XCTestCase {
 
+    func test_load_deliversNoItemsOnEmptyCache() {
+        let sut = makeSUT()
+        
+        let exp = expectation(description: "Wait for load completion")
+        
+        sut.load { result in
+            switch result {
+            case let .success(imageFeed):
+                XCTAssertEqual(imageFeed, [], "Expected empty feed")
+            case let .failure(error):
+                XCTFail("Expected sucessful feed result, got \(error) instead")
+            }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
     
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
+        let storeURL = testSpecificStoreURL()
+        let store = CodableFeedStore(storeURL: storeURL)
+        let sut = LocalFeedLoader(store: store, currentDate: Date())
+        trackForMemoryLeaks(instance: store, file: file, line: line)
+        trackForMemoryLeaks(instance: sut, file: file, line: line)
+        return sut
+    }
+    
+    private func testSpecificStoreURL() -> URL {
+        return cachesDirectory().appendingPathComponent("\(type(of: self)).store")
+    }
+    
+    private func cachesDirectory() -> URL {
+        return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
 
 }
